a2e0ff3c54beceb2a944936f5538628080d3d10f
Adriel  Alexsander <alexander.sousa@smn.com.br>
2024-08-28 15:07:59 -0300

    concertando tela de login e terminando implementação da tela de input e pdf do relatório

--- BudgetGenerator/Controllers/HomeController.cs
+++ BudgetGenerator/Controllers/HomeController.cs
@@ -2,26 +2,9 @@
 using BudgetGenerator.Services;
 using Microsoft.AspNetCore.Mvc;
 
+namespace BudgetGenerator.Controllers;
 
-namespace BudgetGenerator.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
-    {
-        private readonly MatrixService _matrixService;
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _matrixService = new MatrixService();
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-
-    }
+    public IActionResult Index() => View();
 }
